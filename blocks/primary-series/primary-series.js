d9999b870d48aa69b48f7d89fb6d4084d2191107
Katie Sexton <ksexton@adobe.com>
2022-12-16 15:35:30 -0700

    feat: primary series block

---
+++ blocks/primary-series/primary-series.js
@@ -0,0 +1,40 @@
+import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
+
+function decorateDoses(section) {
+  console.log('hi from doses', section);
+  const cols = [...section.firstElementChild.children];
+  // rewrap doses cols
+  cols.forEach((col) => {
+    const wrapper = document.createElement('div');
+    let type;
+    if (col.querySelector('strong')) {
+      type = 'dose';
+      const num = col.innerHTML.split(' ')[0];
+      col.innerHTML = col.innerHTML.replace(num, `<span>${num}</span>`);
+    } else if (col.querySelector('em')) {
+      type = 'days';
+      col.innerHTML = col.textContent;
+    }
+    wrapper.className = `primary-series-doses-${type}`;
+    wrapper.append(col);
+    section.firstElementChild.append(wrapper);
+  });
+}
+
+/**
+ * loads and decorates the primary series
+ * @param {Element} block The primary series block element
+ */
+export default async function decorate(block) {
+  const sections = ['doses', 'info'];
+  sections.forEach((s, i) => {
+    const section = block.children[i];
+    if (section) section.classList.add(`primary-series-${s}`);
+  });
+
+  const doses = block.querySelector('.primary-series-doses');
+  if (doses) decorateDoses(doses);
+
+  // const info = block.querySelector('.primary-series-info');
+  // if (info) decorateInfo(info);
+}
